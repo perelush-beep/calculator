c2cfe09c4ce96b2c9d5463a111952e805e8c0cca
Aharom_Perel <perelush@gmail.com>
2026-02-03 12:30:37 +0200

    added divide func

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ def sub (a, b):
 def multiply(a,b):
     """Multiply teo numbers."""
     return a*b
+def divide (a,b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        print("מה אתה מחלק באפס יא דבע")
